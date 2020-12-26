4cac2ffbd1ab41f8cf8a355de39bd8cab598031c
Ilya <i.serdykow@gmail.com>
2020-12-27 00:10:35 +0300

    New controller

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
@@ -7,8 +9,20 @@ namespace CommandAPI.Controllers
     [ApiController]
         public class CommandsController : ControllerBase
     {
-        public ActionResult<IEnumerable<string>> Get(){
-            return new string[] {"This","is","hard" , "code"};
+        private readonly ICommandAPIRepo _repository;
+        public CommandsController(ICommandAPIRepo repository)
+        {
+            _repository = repository;
         }
+        [HttpGet]
+        ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+           var commandItems = _repository.GetAllCommands();
+
+            return Ok(commandItems);
+            
+        }
+
+
     }
 }
---
+++ src/CommandAPI/Data/ICommandAPIRepo.cs
@@ -0,0 +1,17 @@
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public interface ICommandAPIRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandByID(int id);
+        void CreateCommand(Command cmd);
+        void UpdateCommand(Command cmd);
+        void DeleteCommand(Command cmd);
+         
+    }
+}
